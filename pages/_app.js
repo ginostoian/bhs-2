37a8895b59c772efd54b50cc7090670a816cf4e6
Gino Stoian <ginostoian@gmail.com>
2023-03-25 10:21:58 +0200

    added css for footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,16 @@
-import Navigation from '../components/navigation/Navigation'
-import '../styles/globals.css'
+import Navigation from "../components/navigation/Navigation";
+import "../styles/globals.css";
 
-// import Footer from '../components/footer/Footer'
+import Footer from "../components/footer/Footer";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navigation />
       <Component {...pageProps} />
-      {/* <Footer /> */}
+      <Footer />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
