2188c6c4cb75089137c96b6edead0308b76da422
Gino Stoian <ginostoian@gmail.com>
2023-04-08 23:22:02 +0300

    fix styiling issue with image

--- components/blog/BlogPostHighlight.js
+++ components/blog/BlogPostHighlight.js
@@ -4,24 +4,24 @@ import classes from "./BlogPostHighlight.module.css";
 
 const BlogPostHighlight = () => {
   return (
-    <div class={classes["article-container"]}>
+    <div className={classes["article-container"]}>
       <Image
         loading="lazy"
         src="/assets/img/general/living-room-1.jpeg"
         alt=""
-        class={classes["article__img"]}
-        width={250}
+        className={classes["article__img"]}
+        width={550}
         height={250}
       />
 
-      <div class={classes["article__body"]}>
-        <p class={classes["article__body-date"]}>21 August 2022</p>
-        <h3 class={classes["article__body-title"]}>
+      <div className={classes["article__body"]}>
+        <p className={classes["article__body-date"]}>21 August 2022</p>
+        <h3 className={classes["article__body-title"]}>
           Top 10 trends for renovating your home in 2022
         </h3>
         <a
           href="./pages/blogs/top-10-trends-for-renovating-your-home-in-2022.html"
-          class={`${classes["article__body-btn"]} btn`}
+          className={`${classes["article__body-btn"]} btn`}
         >
           Read more
         </a>
