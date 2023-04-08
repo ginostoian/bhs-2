f894a6d4266102d21591f94ab94f5d8c7a372257
Gino Stoian <ginostoian@gmail.com>
2023-04-09 01:18:02 +0300

    fix attribute camelcasing

--- components/reviews/Reviews.js
+++ components/reviews/Reviews.js
@@ -9,7 +9,7 @@ const Reviews = () => {
       <iframe
         id="testimonialto-carousel-all-better-homes-studio-light"
         src="https://embed-v2.testimonial.to/carousel/all/better-homes-studio?theme=light&autoplay=on&showmore=on&one-row=on&hideDate=on&same-height=off"
-        frameborder="0"
+        frameBorder="0"
         scrolling="yes"
         width="100%"
         height={"600px"}
