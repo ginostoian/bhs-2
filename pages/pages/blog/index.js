f3217e7a569bdff453f628e062692cd657d51a10
Gino Stoian <ginostoian@gmail.com>
2023-12-11 01:40:29 +0200

    add placeholder text for the blog

--- pages/pages/blog/index.js
+++ pages/pages/blog/index.js
@@ -1,5 +1,18 @@
+import Hero from "../../../components/hero/Hero";
+
 const BlogPage = () => {
-  return <div></div>;
+  return (
+    <Hero
+      title={"We're building this page"}
+      titleAccent={"as we speak."}
+      subtitle={
+        "This page will be our knowledge center. Stay tuned for detailed information on costs, trends and advice."
+      }
+      heroCTA={"Get in touch"}
+      heroImgUrl={"misc/congrats.jpg"}
+      ctaTallyFormLink={"/pages/contact"}
+    />
+  );
 };
 
 export default BlogPage;
