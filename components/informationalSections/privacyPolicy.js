beac0af61815c700c585f12b3c1c5fd2ae3de3ba
Gino Stoian <ginostoian@gmail.com>
2023-12-12 02:20:46 +0200

    add analytics and hot jar scripts

---
+++ components/informationalSections/privacyPolicy.js
@@ -0,0 +1,7 @@
+import classes from "TextBlock.module.css";
+
+const privacyPolicy = () => {
+  return <div></div>;
+};
+
+export default privacyPolicy;
--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,31 @@ function MyApp({ Component, pageProps }) {
     <>
       <Script
         async
+        src="https://www.googletagmanager.com/gtag/js?id=G-79F5GFQ1GP"
+      ></Script>
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-79F5GFQ1GP');
+        `}
+      </Script>
+      <Script id="hotjar">
+        {`
+          (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:3786664,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}
+      </Script>
+      <Script
+        async
         id="tally-js"
         src="https://tally.so/widgets/embed.js"
         onLoad={() => {
