9416dae70128552dd70a383c1fe736f9dca963ed
Gino Stoian <ginostoian@gmail.com>
2023-12-12 02:41:38 +0200

    placeholder text

--- components/informationalSections/privacyPolicy.js
+++ components/informationalSections/privacyPolicy.js
@@ -1,7 +1,7 @@
 import classes from "TextBlock.module.css";
 
 const privacyPolicy = () => {
-  return <div></div>;
+  return <div>Under construction. Will be up in 48 hours</div>;
 };
 
 export default privacyPolicy;
