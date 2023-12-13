a06c3de941b8c07d47c306b27394677fda73aac6
Gino Stoian <ginostoian@gmail.com>
2023-12-14 01:58:45 +0200

    change script to next Script and make iframe async

--- components/reviews/Reviews.js
+++ components/reviews/Reviews.js
@@ -1,12 +1,15 @@
+import Script from "next/script";
+
 const Reviews = () => {
   return (
     <div className="container">
-      <script
+      <Script
+        async
         type="text/javascript"
         src="https://testimonial.to/js/iframeResizer.min.js"
-        defer
-      ></script>
+      ></Script>
       <iframe
+        loading="lazy"
         id="testimonialto-carousel-all-better-homes-studio-light"
         src="https://embed-v2.testimonial.to/carousel/all/better-homes-studio?theme=light&autoplay=on&showmore=on&one-row=on&hideDate=on&same-height=off"
         frameBorder="0"
