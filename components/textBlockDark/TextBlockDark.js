e9601b1695277d0e16be8dfd6e3622e2366e5bb3
Gino Stoian <ginostoian@gmail.com>
2024-09-12 12:44:50 +0300

    modify cta

--- components/textBlockDark/TextBlockDark.js
+++ components/textBlockDark/TextBlockDark.js
@@ -15,7 +15,7 @@ const TextBlockDark = ({ content }) => {
         </p>
         <Link
           href="/contact"
-          className="service__btn btn border-none"
+          className="w-max flex items-center justify-center mx-auto transition duration-200 cursor-pointer font-bold border-2 bg-[#266bf1] capitalize text-white border-transparent hover:bg-[#1449B0] hover:text-gray-50 active:bg-[#0C5AC8] disabled:bg-[#A5D2FF] text-[18px] px-[20px] min-h-[64px] lg:min-h-[72px] lg:px-[24px] lg:w-[245px]! rounded-[16px]"
         >
           {content.darkBgTextBtn}
         </Link>
