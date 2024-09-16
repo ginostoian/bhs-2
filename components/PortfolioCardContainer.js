c1a830242d0d0d008b0160dd6d63b6fadf79e5c1
Gino Stoian <ginostoian@gmail.com>
2024-09-17 02:16:05 +0300

    add portfolio section and fix css styles

--- components/PortfolioCardContainer.js
+++ components/PortfolioCardContainer.js
@@ -3,11 +3,36 @@ import PortfolioCard from "./PortfolioCard";
 const PortfolioCardContainer = () => {
   return (
     <div className="container flex flex-row flex-wrap gap-12 justify-center">
-      <PortfolioCard />
-      <PortfolioCard />
-      <PortfolioCard />
-      <PortfolioCard />
-      <PortfolioCard />
+      <PortfolioCard
+        imgURL="/assets/img/bathroom/dark-bold-colours-in-small-bathroom.jpeg"
+        imgAlt="black and gold luxury bathroom"
+        title="Rosa's bathroom renovation"
+        desc="A stunning refurbishment of a bathroom in SW15. Bold colours for a bold personality"
+      />
+      <PortfolioCard
+        imgURL="/assets/img/bathroom/bathroom-design.webp"
+        imgAlt="black and gold luxury bathroom"
+        title="Rosa's bathroom renovation"
+        desc="A stunning refurbishment of a bathroom in SW15. Bold colours for a bold personality"
+      />
+      <PortfolioCard
+        imgURL="/assets/img/bathroom/bathroom-photo.webp"
+        imgAlt="black and gold luxury bathroom"
+        title="Rosa's bathroom renovation"
+        desc="A stunning refurbishment of a bathroom in SW15. Bold colours for a bold personality"
+      />
+      <PortfolioCard
+        imgURL="/assets/img/bathroom/industrial-bathroom.webp"
+        imgAlt="black and gold luxury bathroom"
+        title="Rosa's bathroom renovation"
+        desc="A stunning refurbishment of a bathroom in SW15. Bold colours for a bold personality"
+      />
+      <PortfolioCard
+        imgURL="/assets/img/bathroom/contemporary-bathroom.webp"
+        imgAlt="black and gold luxury bathroom"
+        title="Rosa's bathroom renovation"
+        desc="A stunning refurbishment of a bathroom in SW15. Bold colours for a bold personality"
+      />
     </div>
   );
 };
