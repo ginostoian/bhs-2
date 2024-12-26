1de5eabf5bc93ff22075b49fe4445e0db0e2b8a5
Gino Stoian <ginostoian@gmail.com>
2024-12-26 02:12:14 +0200

    footer changes

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -63,10 +63,7 @@ const Footer = () => {
 
           <div className={classes["footer__body-social-wrapper"]}>
             <div className={classes["social-media"]}>
-              <a
-                href="http://twitter.com"
-                target="_blank"
-              >
+              <a href="http://twitter.com" target="_blank">
                 <Image
                   className={classes["social-icon"]}
                   src="/assets/icons/twitter-icon.svg"
@@ -93,10 +90,7 @@ const Footer = () => {
             </div>
 
             <div className={classes["social-media"]}>
-              <a
-                href="https://www.facebook.com/bhomes.studio"
-                target="_blank"
-              >
+              <a href="https://www.facebook.com/bhomes.studio" target="_blank">
                 <Image
                   className={classes["social-icon"]}
                   src="/assets/icons/facebook-icon.svg"
@@ -146,28 +140,16 @@ const Footer = () => {
               >
                 Renovation
               </Link>
-              <Link
-                href="/about"
-                className={classes["footer__quarter-link"]}
-              >
+              <Link href="/about" className={classes["footer__quarter-link"]}>
                 About us
               </Link>
-              <Link
-                href="/blog"
-                className={classes["footer__quarter-link"]}
-              >
+              <Link href="/blog" className={classes["footer__quarter-link"]}>
                 Blog
               </Link>
-              <Link
-                href="/faq"
-                className={classes["footer__quarter-link"]}
-              >
+              <Link href="/faq" className={classes["footer__quarter-link"]}>
                 FAQ
               </Link>
-              <Link
-                href="/contact"
-                className={classes["footer__quarter-link"]}
-              >
+              <Link href="/contact" className={classes["footer__quarter-link"]}>
                 Contact
               </Link>
               <Link
@@ -235,18 +217,37 @@ const Footer = () => {
 
         <div className={classes["footer__body-copyright"]}>
           <div>
-            &copy; {new Date().getFullYear()} - The Wisehome Group Limited
-            trading as Better Homes Studio
+            Copyright &copy; {new Date().getFullYear()} - The Wisehome Group
+            Limited
           </div>
+          <div>Trading as Better Homes Studio</div>
           <div className="dot-divider"></div>
           <div>
-            made by{" "}
             <a
-              className={classes["footer__copyright-link"]}
               href="https://ginostoian.com"
+              title="Go to Gino's website"
               target="_blank"
+              className="inline-block cursor-pointer rounded bg-neutral px-2 py-1 text-sm text-neutral-content ring-1 ring-base-content/10 duration-200 hover:ring-neutral"
             >
-              gnst_o
+              <div className="flex items-center gap-1">
+                <span className="opacity-90">Built by</span>
+                <span className="flex items-center gap-0.5 font-semibold tracking-tight">
+                  <svg
+                    className="size-5"
+                    viewBox="0 0 375 509"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M233.962 11.7151L233.954 11.7229L186.393 57.3942L186.392 57.3948C116.335 124.657 57.1377 202.349 10.9069 287.707L10.8624 287.789L10.8164 287.87C10.5281 288.38 10.3791 288.954 10.383 289.537C10.387 290.12 10.5438 290.693 10.839 291.198C11.1342 291.704 11.5582 292.125 12.0701 292.419C12.5819 292.713 13.1633 292.868 13.756 292.869H129.042H139.042V302.869V494.875V494.888C139.042 495.535 139.229 496.17 139.584 496.715L131.361 502.072L139.584 496.715C139.939 497.26 140.447 497.692 141.048 497.957C141.648 498.222 142.314 498.308 142.963 498.202C143.613 498.096 144.215 497.804 144.698 497.365L144.7 497.363L165.966 477.999L165.97 477.996C239.677 410.959 302.226 332.637 351.272 245.969L351.274 245.966L364.435 222.73L364.44 222.721L364.445 222.712C364.735 222.203 364.885 221.627 364.882 221.043C364.879 220.459 364.723 219.886 364.427 219.379C364.132 218.872 363.707 218.45 363.194 218.156C362.681 217.862 362.099 217.707 361.505 217.707H361.5H249.685H239.685V207.707V14.1248C239.685 13.47 239.492 12.8285 239.129 12.28M233.962 11.7151L239.129 12.28M233.962 11.7151C234.438 11.2571 235.04 10.9473 235.694 10.8267C236.349 10.7061 237.024 10.7805 237.635 11.0399C238.246 11.2993 238.765 11.7314 239.129 12.28M233.962 11.7151L247.465 6.75675L239.129 12.28"
+                      fill="#FFBE18"
+                      stroke="black"
+                      strokeWidth="20"
+                    />
+                  </svg>
+                  Gino
+                </span>
+              </div>
             </a>
           </div>
         </div>
--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -40,7 +40,7 @@ const Hero = ({
         <Link
           href={ctaTallyFormLink || "/contact"}
           // className={`${classes["hero__btn"]}`}
-          className="w-full mb-10 flex items-center justify-center transition duration-200 cursor-pointer font-bold border-2 bg-[#266bf1] capitalize text-white border-transparent hover:bg-[#1449B0] hover:text-gray-50 active:bg-[#0C5AC8] disabled:bg-[#A5D2FF] text-[18px] px-[20px] min-h-[64px] lg:min-h-[72px] lg:px-[24px] lg:w-[245px]! rounded-full"
+          className="lg:w-[245px]! mb-10 flex min-h-[64px] w-full cursor-pointer items-center justify-center rounded-full border-2 border-transparent bg-[#266bf1] px-[20px] text-[18px] font-bold capitalize text-white transition duration-200 hover:bg-[#1449B0] hover:text-gray-50 active:bg-[#0C5AC8] disabled:bg-[#A5D2FF] lg:min-h-[72px] lg:px-[24px]"
         >
           {heroCTA}
         </Link>
