3bf182415fac54746ed8cdcceb54cf67e2e1218f
Gino Stoian <ginostoian@gmail.com>
2025-09-05 00:22:59 +0300

    copy changes

--- app/partner-with-us/page.js
+++ app/partner-with-us/page.js
@@ -1,35 +1,19 @@
 import Approach from "@/components/Approach";
-import CustomCTA from "@/components/CustomCTA";
 import FAQ from "@/components/FAQ";
 import Features from "@/components/Features";
 import Guarantee from "@/components/Guarantee";
 import InfoPartner from "@/components/InfoPartner";
-import PortfolioCardContainer from "@/components/PortfolioCardContainer";
 import Stats from "@/components/Stats";
 import Testimonials11 from "@/components/Testimonials11";
-import Testimonials3 from "@/components/Testimonials3";
 import WhoCanPartner from "@/components/WhoCanPartner";
-import BlogHighlight from "@/components/blog/BlogHighlight";
-import ContentRow from "@/components/contentRow/ContentRow";
 import Hero from "@/components/hero/Hero";
 import SectionTitle from "@/components/sectionTitle/SectionTitle";
-import SocialProof from "@/components/socialProof/SocialProof";
-import TextBlockDark from "@/components/textBlockDark/TextBlockDark";
 import TextGrid from "@/components/textGrid/TextGrid";
 import config from "@/config";
 import Link from "next/link";
 
 export default function Page() {
   const bathroomPageCopy = config.copy.bathroomPage;
-  const whatWeDoCopy = bathroomPageCopy.whatWeDoSection;
-  const whatWeDoArr = [
-    whatWeDoCopy.bathroomDesign,
-    whatWeDoCopy.modernBathroom,
-    whatWeDoCopy.industrialBathroom,
-    whatWeDoCopy.traditionalBathroom,
-    whatWeDoCopy.smallBathroom,
-    whatWeDoCopy.victorianBathroom,
-  ];
   const howWeDoItCopy = bathroomPageCopy.howWeDoItSection;
 
   const faqs = [
@@ -129,9 +113,9 @@ export default function Page() {
     <>
       <main>
         <Hero
-          title="Tap into the Better Homes Studio Network."
+          title="Tap into the Better Homes Network."
           titleAccent="Unlock new revenue opportunities."
-          subtitle="Refer your clients to our trusted renovation services, and we will reward you for every successful project—ensuring both you and your clients benefit from our expertise. It is a simple, rewarding partnership designed for mutual success."
+          subtitle="Refer your clients to our trusted renovation services, and we will reward you for every successful project-ensuring both you and your clients benefit from our expertise. You could earn up to £3,000 for every successful referral"
           heroCTA="Start Application - Start earning more"
           heroImgUrl="professionals-hero.webp"
           ctaTallyFormLink="https://tally.so/r/nPvjxV"
--- components/WhoCanPartner.js
+++ components/WhoCanPartner.js
@@ -36,7 +36,7 @@ const WhoCanPartner = () => {
             {/* <!-- List --> */}
             <ul className="space-y-2 sm:space-y-4">
               <li className="flex gap-x-3">
-                <span className="size-5 mt-0.5 flex items-center justify-center rounded-full bg-blue-50 text-blue-600">
+                <span className="mt-0.5 flex size-5 items-center justify-center rounded-full bg-blue-50 text-blue-600">
                   <svg
                     className="size-3.5 shrink-0"
                     xmlns="http://www.w3.org/2000/svg"
@@ -60,7 +60,7 @@ const WhoCanPartner = () => {
               </li>
 
               <li className="flex gap-x-3">
-                <span className="size-5 mt-0.5 flex items-center justify-center rounded-full bg-blue-50 text-blue-600">
+                <span className="mt-0.5 flex size-5 items-center justify-center rounded-full bg-blue-50 text-blue-600">
                   <svg
                     className="size-3.5 shrink-0"
                     xmlns="http://www.w3.org/2000/svg"
@@ -84,7 +84,7 @@ const WhoCanPartner = () => {
               </li>
 
               <li className="flex gap-x-3">
-                <span className="size-5 mt-0.5 flex items-center justify-center rounded-full bg-blue-50 text-blue-600">
+                <span className="mt-0.5 flex size-5 items-center justify-center rounded-full bg-blue-50 text-blue-600">
                   <svg
                     className="size-3.5 shrink-0"
                     xmlns="http://www.w3.org/2000/svg"
@@ -108,7 +108,7 @@ const WhoCanPartner = () => {
               </li>
 
               <li className="flex gap-x-3">
-                <span className="size-5 mt-0.5 flex items-center justify-center rounded-full bg-blue-50 text-blue-600">
+                <span className="mt-0.5 flex size-5 items-center justify-center rounded-full bg-blue-50 text-blue-600">
                   <svg
                     className="size-3.5 shrink-0"
                     xmlns="http://www.w3.org/2000/svg"
@@ -132,7 +132,7 @@ const WhoCanPartner = () => {
               </li>
 
               <li className="flex gap-x-3">
-                <span className="size-5 mt-0.5 flex items-center justify-center rounded-full bg-blue-50 text-blue-600">
+                <span className="mt-0.5 flex size-5 items-center justify-center rounded-full bg-blue-50 text-blue-600">
                   <svg
                     className="size-3.5 shrink-0"
                     xmlns="http://www.w3.org/2000/svg"
@@ -155,7 +155,7 @@ const WhoCanPartner = () => {
                 </div>
               </li>
               <li className="flex gap-x-3">
-                <span className="size-5 mt-0.5 flex items-center justify-center rounded-full bg-blue-50 text-blue-600">
+                <span className="mt-0.5 flex size-5 items-center justify-center rounded-full bg-blue-50 text-blue-600">
                   <svg
                     className="size-3.5 shrink-0"
                     xmlns="http://www.w3.org/2000/svg"
@@ -178,7 +178,7 @@ const WhoCanPartner = () => {
                 </div>
               </li>
               <li className="flex gap-x-3">
-                <span className="size-5 mt-0.5 flex items-center justify-center rounded-full bg-blue-50 text-blue-600">
+                <span className="mt-0.5 flex size-5 items-center justify-center rounded-full bg-blue-50 text-blue-600">
                   <svg
                     className="size-3.5 shrink-0"
                     xmlns="http://www.w3.org/2000/svg"
@@ -197,6 +197,30 @@ const WhoCanPartner = () => {
                 <div className="grow">
                   <span className="text-sm text-gray-500 sm:text-base">
                     Other building & renovating companies
+                  </span>
+                </div>
+              </li>
+              <li className="flex gap-x-3">
+                <span className="mt-0.5 flex size-5 items-center justify-center rounded-full bg-blue-50 text-blue-600">
+                  <svg
+                    className="size-3.5 shrink-0"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <polyline points="20 6 9 17 4 12" />
+                  </svg>
+                </span>
+                <div className="grow">
+                  <span className="text-sm text-gray-500 sm:text-base">
+                    Really anyone with a network or domestic clients that want
+                    to introduce us to their friends or family.
                   </span>
                 </div>
               </li>
