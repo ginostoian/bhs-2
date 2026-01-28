03aa25a08b9f9f6f08254a94c12dc5fbaa89e1e9
Gino Stoian <ginostoian@gmail.com>
2026-01-29 00:49:15 +0200

    replace copy to align with new style

--- components/CustomCTA.js
+++ components/CustomCTA.js
@@ -7,8 +7,7 @@ const CustomCTA = () => {
     <section className="mx-auto flex max-w-[85%] flex-col justify-around gap-10 rounded-xl bg-white px-10 py-10 xl:flex-row xl:items-center 2xl:gap-32">
       <div className="">
         <h2 className="mb-8 max-w-2xl text-2xl font-extrabold text-[#100b47] md:text-4xl lg:mx-auto">
-          Stress-Free Renovations: On Time, On Budget, and Exceeding
-          Expectations
+          Premium Renovations and Home Extensions: On Time, On Budget
         </h2>
         <div className="flex max-w-2xl flex-col rounded-xl bg-[#f0f0f0] px-10 py-14 md:flex-row md:items-center md:justify-between lg:mx-auto">
           <div>
@@ -17,15 +16,15 @@ const CustomCTA = () => {
             </h3>
             <div className="flex gap-4">
               <div className="mb-8">
-                <p className="text-sm">Get an additional</p>
-                <p className="text-xl text-[#266bf1]">up to £3,000 off</p>
+                <p className="text-sm">Limited availability</p>
+                <p className="text-xl text-[#266bf1]">Increase your home's value</p>
               </div>
               <div>
                 <Image
                   src="/assets/illustrations/discount animation.png"
                   width={50}
                   height={50}
-                  alt="Get £3,000 off your project with Better Homes Studio"
+                  alt="Book a consultation with Better Homes Studio"
                 />
               </div>
             </div>
@@ -36,7 +35,7 @@ const CustomCTA = () => {
               target="_blank"
               className="lg:w-[245px]! btn-shadow flex min-h-[64px] w-max cursor-pointer items-center justify-center rounded-full border-2 border-[#266bf1] bg-[#266bf1] px-[26px] text-[18px] font-bold leading-[140%] text-white transition duration-200 hover:border-transparent hover:bg-[#1449B0] hover:text-gray-50 active:bg-[#0C5AC8] disabled:bg-[#A5D2FF] lg:min-h-[48px] lg:px-[24px]"
             >
-              Get your Discount
+              Book a Consultation
             </Link>
           </div>
         </div>
