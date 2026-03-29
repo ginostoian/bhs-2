55cbc17490a9fdaeaedd4d34485eb5ccce3ae8b4
Gino Stoian <ginostoian@gmail.com>
2026-03-30 00:47:50 +0300

    replaced star with icon

--- components/TestimonialsAvatars.js
+++ components/TestimonialsAvatars.js
@@ -14,6 +14,17 @@ const StarRow = () => (
   </div>
 );
 
+const PillStar = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+    className="h-4 w-4 text-[#f4b400]"
+  >
+    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81H7.03a1 1 0 00.951-.69l1.07-3.292z" />
+  </svg>
+);
+
 const TrustPill = ({ children }) => (
   <div className="inline-flex items-center rounded-full border border-[#d8e4fb] bg-white px-4 py-2 text-sm font-semibold text-[#100b47] shadow-sm">
     {children}
@@ -38,8 +49,14 @@ const TestimonialsAvatars = () => {
         </div>
 
         <div className="flex flex-wrap gap-2">
-          <TrustPill>Google 5 Stars</TrustPill>
-          <TrustPill>My Builder 5 Stars</TrustPill>
+          <TrustPill>
+            <span className="mr-2">Google 5</span>
+            <PillStar />
+          </TrustPill>
+          <TrustPill>
+            <span className="mr-2">My Builder 5</span>
+            <PillStar />
+          </TrustPill>
           <TrustPill>Best of Houzz Award</TrustPill>
         </div>
       </div>
